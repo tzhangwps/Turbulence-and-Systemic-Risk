bc244868f8a013e78eb6c3652b1a2c434a7296e2
Terrence Y. Zhang <46796143+tzhangwps@users.noreply.github.com>
2019-05-18 21:47:09 -0400

    Add files via upload

--- TurbulenceSuite.py
+++ TurbulenceSuite.py
@@ -26,15 +26,22 @@ import turbulence_suite as ts
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 parser = argparse.ArgumentParser()
-parser.add_argument('api_key', type=str,
+parser.add_argument('-a', '--api_key', type=str,
                     help=
                     '''
                     The api key used to pull data from Alpha Vantage.
                     
                     Get your api key here: https://www.alphavantage.co/support/#api-key
                     ''')
+parser.add_argument('-d', '--drop_recent', type=int,
+                    help=
+                    """
+                    How many rows do you want to remove from the "prices"
+                    dataframe (reverse-chronological)?
+                    """)
 args = parser.parse_args()
 api_key = args.api_key
+rows_to_drop = args.drop_recent
 
 prices_path = os.path.join(os.getcwd(), 'index_data.pkl')
 turbulence_chart_path = os.path.join(os.getcwd(), 'turbulence_chart.csv')
@@ -45,38 +52,47 @@ systemic_risk_chart_path = os.path.join(os.getcwd(), 'systemic_risk_chart.csv')
 ###############################################################################
 
 prices = pd.read_pickle(prices_path)
-print('\nRequesting data from Alpha Vantage API...')
-prices = ts.MainProcess().update_weekly_prices(prices, api_key=api_key)
-prices.to_pickle(prices_path)
-
-prices = ts.MainProcess().add_curve_slope(prices)
-returns = ts.MainProcess().calculate_returns(prices)
-
-print('\nBuilding Turbulence Index...')
-turbulence = pd.DataFrame(ts.MainProcess().calculate_turbulence(returns))
-print('Turbulence Index completed!')
-print('\nBuilding Systemic Risk Index...')
-systemic_risk = pd.DataFrame(ts.MainProcess().calculate_systemic_risk(returns))
-print('Systemic Risk Index completed!')
-
-turbulence_chart = pd.DataFrame({
-                                 'Dates': ['date'] + list(turbulence['Dates']),
-                                 'Raw Turbulence': ['number'] + list(turbulence['Raw Turbulence']),
-                                 'Turbulence': ['number'] + list(turbulence['Turbulence']),
-                                })
-systemic_risk_chart = pd.DataFrame({
-                                    'Dates': ['date'] + list(systemic_risk['Dates']),
-                                    'Systemic Risk': ['number'] + list(systemic_risk['Systemic Risk'])
-                                   })
-
-turbulence_chart.to_csv(turbulence_chart_path, index=False)
-systemic_risk_chart.to_csv(systemic_risk_chart_path, index=False)
-
-print('\nTurbulence and Systemic Risk data written as .csv files and saved to',
-      str(os.getcwd()))
-print('\n{}'.format(turbulence.iloc[-5:,]))
-input('Press [ENTER] to close the program.')
-        
+if rows_to_drop is not None:
+    prices = prices.iloc[rows_to_drop:,]
+    prices.reset_index(inplace=True)
+    prices = prices.drop('index', axis=1)
+    prices.to_pickle(prices_path)
+    
+    print('\nRemoved {} rows from "prices" dataframe'.format(rows_to_drop),
+          'and saved to {}'.format(os.getcwd()))
+elif api_key is not None:
+    print('\nRequesting data from Alpha Vantage API...')
+    prices = ts.MainProcess().update_weekly_prices(prices, api_key=api_key)
+    prices.to_pickle(prices_path)
+    
+    prices = ts.MainProcess().add_curve_slope(prices)
+    returns = ts.MainProcess().calculate_returns(prices)
+    
+    print('\nBuilding Turbulence Index...')
+    turbulence = pd.DataFrame(ts.MainProcess().calculate_turbulence(returns))
+    print('Turbulence Index completed!')
+    print('\nBuilding Systemic Risk Index...')
+    systemic_risk = pd.DataFrame(ts.MainProcess().calculate_systemic_risk(returns))
+    print('Systemic Risk Index completed!')
+    
+    turbulence_chart = pd.DataFrame({
+                                     'Dates': ['date'] + list(turbulence['Dates']),
+                                     'Raw Turbulence': ['number'] + list(turbulence['Raw Turbulence']),
+                                     'Turbulence': ['number'] + list(turbulence['Turbulence']),
+                                    })
+    systemic_risk_chart = pd.DataFrame({
+                                        'Dates': ['date'] + list(systemic_risk['Dates']),
+                                        'Systemic Risk': ['number'] + list(systemic_risk['Systemic Risk'])
+                                       })
+    
+    turbulence_chart.to_csv(turbulence_chart_path, index=False)
+    systemic_risk_chart.to_csv(systemic_risk_chart_path, index=False)
+    
+    print('\nTurbulence and Systemic Risk data written as .csv files and saved to',
+          str(os.getcwd()))
+    print('\n{}'.format(turbulence.iloc[-5:,]))
+    input('Press [ENTER] to close the program.')
+            
 #MIT License
 #
 #Copyright (c) 2019 Terrence Y. Zhang
